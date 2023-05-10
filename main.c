f3913b9ce77a47cc57c7c0d3db0d4f03e623d399
Ela Mayrench <ela.mayrench@campus.technion.ac.il>
2023-05-10 16:54:43 +0300

    working main

--- main.c
+++ main.c
@@ -1,21 +1,26 @@
 #include "HackEnrollment.h"
+#include <string.h>
 
 int main(int argc, char** argv){
+
     int index=0;
     bool flag = false;
     
-    if(argc>5){
+
+    if(argc > 6){
         index = 1;
         flag = true;
     }
 
-    FILE* students_file = fopen(argv[0+index],"r");
-    FILE* courses_file = fopen(argv[1+index],"r");
-    FILE* hackers_file = fopen(argv[2+index],"r");
-    FILE* queues_file = fopen(argv[3+index],"r");
-    FILE* target_file = fopen(argv[4+index],"w");
+    FILE* students_file = fopen(argv[1+index],"r");
+    FILE* courses_file = fopen(argv[2+index],"r");
+    FILE* hackers_file = fopen(argv[3+index],"r");
+    FILE* queues_file = fopen(argv[4+index],"r");
+    FILE* target_file = fopen(argv[5+index],"w");
+
 
     EnrollmentSystem sys = createEnrollment(students_file,courses_file,hackers_file);
+
     SetFlag(sys,flag);
 
     fclose(students_file);
@@ -29,7 +34,7 @@ int main(int argc, char** argv){
     hackEnrollment(sys,target_file);
     fclose(target_file);
     
-    //need to do: 1)destroy sys 2)use i
+    DestroyEnrollmentSystem(sys);
 
 return 0;
 }
